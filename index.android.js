761683fdb7606c62c32ad2a32f847d662f89a5df
vivihuang <rwhuang@thoughtworks.com>
2016-05-05 12:54:13 +0800

    build the app followed the guide for react-native

--- index.android.js
+++ index.android.js
@@ -2,50 +2,89 @@
  * Sample React Native App
  * https://github.com/facebook/react-native
  */
+var REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json'
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import React, { Component } from 'react'
+import { AppRegistry, Image, StyleSheet, Text, View } from 'react-native'
 
 class AwesomeProject extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { movies: null }
+    this.fetchData = this.fetchData.bind(this)
+  }
+
+  componentDidMount() {
+    this.fetchData()
+  }
+
+  fetchData() {
+    fetch(REQUEST_URL)
+      .then((response) => response.json())
+      .then((responseData) => {
+        console.log(responseData)
+        this.setState({
+          movies: responseData.movies
+        })
+        console.log(this.state)
+      })
+      .done()
+  }
+
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
+    if (!this.state.movies) {
+      return renderLoadingView()
+    }
+
+    var movie = this.state.movies[0]
+    return renderMovie(movie)
   }
 }
+
+const renderLoadingView = () => (
+  <View style={styles.container}>
+    <Text>
+      Loading movies..
+    </Text>
+  </View>
+)
+
+const renderMovie = (movie) => (
+   <View style={styles.container}>
+     <Image
+       source={{uri: movie.posters.thumbnail}}
+       style={styles.thumbnail}
+     />
+     <View style={styles.rightContainer}>
+       <Text style={styles.title}>{movie.title}</Text>
+       <Text style={styles.year}>{movie.year}</Text>
+     </View>
+   </View>
+)
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#F5FCFF'
   },
-  welcome: {
+  rightContainer: {
+    flex: 1
+  },
+  title: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    marginBottom: 8,
+    textAlign: 'center'
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  year: {
+    textAlign: 'center'
   },
-});
+  thumbnail: {
+    width: 53,
+    height: 81
+  }
+})
 
-AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
+AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject)
